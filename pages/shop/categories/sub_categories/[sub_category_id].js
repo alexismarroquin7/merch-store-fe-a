257d78292debf8ccf3c51c4ae2387c90c543ab07
alexismarroquin7 <alexismarroquin754@gmail.com>
2022-01-24 17:22:24 -0800

    used correct naming params

---
+++ pages/shop/categories/sub_categories/[sub_category_id].js
@@ -0,0 +1,10 @@
+import { useRouter } from "next/router"
+
+export default function SubCategory(){
+  const router = useRouter();  
+  return (
+  <div>
+    {router.asPath}
+  </div>
+  )
+}
---
+++ pages/shop/categories/sub_categories/index.js
@@ -0,0 +1,10 @@
+import { useRouter } from "next/router"
+
+export default function SubCategories(){
+  const router = useRouter();  
+  return (
+  <div>
+    {router.asPath}
+  </div>
+  )
+}
